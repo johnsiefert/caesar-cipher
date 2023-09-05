5ae4e5ba2dcd149d123037c4cfffde6b4ab91a4d
johnsiefert <91295870+johnsiefert@users.noreply.github.com>
2023-09-05 23:29:51 +0000

    refactored the code combined encrypt and decrypt

--- main.py
+++ main.py
@@ -4,27 +4,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+      shift_amount *= -1
+  for letter in start_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
-  
+    end_text += alphabet[new_position]
+  print(f"Here's the {direction}d result: {end_text}")
 
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
+caesar(start_text= text, shift_amount= shift, cipher_direction= direction)
 
-
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
 
   
